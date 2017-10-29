4058c322b72c1c217b84029525c32ebc21098a0c
Maren Collier <ytterbo@gmail.com>
2017-10-29 16:12:45 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+var navbar = document.getElementById("nav-bar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
