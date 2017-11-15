0b2a8b566979e627471361283102aaed6b55b108
Maren Collier <ytterbo@gmail.com>
2017-11-15 16:44:34 +0100

    Add logo for linkedIn

---
+++ javascript/sticky_nav.js
@@ -0,0 +1,11 @@
+/*
+FILE NAME: sticky_nav.js
+WRITTEN BY: Maren Collier Ytterbø
+WHEN: November 2017
+PURPOSE: Make navigation bar sticky to prevent from overlapping header
+*/
+
+var navbar = document.getElementById("sticky-nav");
+
+var stick = navbar.offsetTop;
+console.log(stick);
